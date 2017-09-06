5aa8effe632900c3343cb4b50b5f9f1e492568e1
Rubenia-Borge <rubeniaolaska@gmail.com>
2017-09-06 00:58:14 -0600

    Add files via upload

---
+++ First.py
@@ -0,0 +1,38 @@
+print(5/8)
+print(7+10)
+print(100 + 100)
+print(300 - 300)
+print(45 * 2)
+print(800 / 2)
+print(5 ** 3)
+print(100*1.1**7)
+print(21 % 2)
+cravings = 1000
+actor = 2.2
+resulted= cravings*actor**9
+print(resulted)
+
+saved = 100000
+factorized = 505
+resulted= saved*factorized**5
+print(resulted)
+
+desc = "compound interest"
+profitable = False
+
+saving = 1000
+fact = 3.3
+desc = "compound interest"
+year100 = fact * saving
+print(type(year100))
+
+
+doubledesc = desc + desc
+print(doubledesc)
+
+
+savings = 100
+result = 100 * 1.10 ** 7
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+py_string = "3.1415926"
+py_float = float(py_string)
